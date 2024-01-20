18db4c3ebef060b2c98ef42d2150517afe9c4cb8
degencodebeast <degencodebeast@protonmail.com>
2024-01-20 15:23:23 +0100

    added firebase dependencies

--- firebase.js
+++ firebase.js
@@ -9,7 +9,13 @@ import { getFirestore, collection } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
